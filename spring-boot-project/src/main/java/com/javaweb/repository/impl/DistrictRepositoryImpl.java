14a4a295f3afa527400620729032da44f48d6ab6
dung.ta226031@sis.hust.edu.vn <011035788Aa-+->
2024-03-17 18:59:56 +0700

    implemented DistrictRepo findAll() and RentAreaRepo findAll()

--- spring-boot-project/src/main/java/com/javaweb/repository/impl/DistrictRepositoryImpl.java
+++ spring-boot-project/src/main/java/com/javaweb/repository/impl/DistrictRepositoryImpl.java
@@ -1,17 +1,52 @@
 package com.javaweb.repository.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
 
 import com.javaweb.repository.DistrictRepository;
 import com.javaweb.repository.entity.BuildingEntity;
 import com.javaweb.repository.entity.DistrictEntity;
+import com.javaweb.utils.ConnectionUtil;
 
-public class DistrictRepositoryImpl implements DistrictRepository{
+@Repository
+public class DistrictRepositoryImpl implements DistrictRepository {
 
 	@Override
 	public List<DistrictEntity> findAll(List<BuildingEntity> buildings) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sql = new StringBuilder("SELECT\r\n" + "  *\r\n" + "FROM\r\n" + "  district");
+		StringBuilder condition = new StringBuilder("\nWHERE 1 = 1 AND buildingid IN (");
+
+		for (BuildingEntity entity : buildings) {
+			condition.append(entity.getId()).append(", ");
+		}
+		condition.delete(condition.length() - 2, condition.length());
+		condition.append(")");
+
+		sql.append(condition);
+		System.out.println(sql);
+		List<DistrictEntity> results = new ArrayList<>();
+		try (Connection con = ConnectionUtil.getConnection();
+				PreparedStatement stm = con.prepareStatement(sql.toString());
+				ResultSet rs = stm.executeQuery();) {
+			while (rs.next()) {
+				DistrictEntity de = new DistrictEntity();
+				de.setId(rs.getLong("id"));
+				de.setCode(rs.getString("code"));
+				de.setName(rs.getString("name"));
+				results.add(de);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+
+		return results;
 	}
 
 }
--- spring-boot-project/src/main/java/com/javaweb/repository/impl/RentAreaRepositoryImpl.java
+++ spring-boot-project/src/main/java/com/javaweb/repository/impl/RentAreaRepositoryImpl.java
@@ -1,17 +1,52 @@
 package com.javaweb.repository.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
 
 import com.javaweb.repository.RentAreaRepository;
 import com.javaweb.repository.entity.BuildingEntity;
 import com.javaweb.repository.entity.RentAreaEntity;
+import com.javaweb.utils.ConnectionUtil;
 
+@Repository
 public class RentAreaRepositoryImpl implements RentAreaRepository {
 
 	@Override
 	public List<RentAreaEntity> findAll(List<BuildingEntity> buildings) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sql = new StringBuilder("SELECT\r\n" + "  *\r\n" + "FROM\r\n" + "  rentarea");
+		StringBuilder condition = new StringBuilder("\nWHERE 1 = 1 AND buildingid IN (");
+
+		for (BuildingEntity entity : buildings) {
+			condition.append(entity.getId()).append(", ");
+		}
+		condition.delete(condition.length() - 2, condition.length());
+		condition.append(")");
+
+		sql.append(condition);
+		System.out.println(sql);
+		List<RentAreaEntity> results = new ArrayList<>();
+		try (Connection con = ConnectionUtil.getConnection();
+				PreparedStatement stm = con.prepareStatement(sql.toString());
+				ResultSet rs = stm.executeQuery();) {
+			while (rs.next()) {
+				RentAreaEntity ra = new RentAreaEntity();
+				ra.setId(rs.getLong("id"));
+				ra.setValue(rs.getInt("value"));
+				ra.setBuildingId(rs.getLong("buildingid"));
+				results.add(ra);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+
+		return results;
 	}
 
 }
