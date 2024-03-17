a20ef6fb75d69619dee846e49a062cd43d840e34
dung.ta226031@sis.hust.edu.vn <011035788Aa-+->
2024-03-17 18:37:10 +0700

    add District and RentArea repo, implemented the BuildingService

--- spring-boot-project/src/main/java/com/javaweb/repository/impl/DistrictRepositoryImpl.java
+++ spring-boot-project/src/main/java/com/javaweb/repository/impl/DistrictRepositoryImpl.java
@@ -1,7 +1,17 @@
 package com.javaweb.repository.impl;
 
+import java.util.List;
+
 import com.javaweb.repository.DistrictRepository;
+import com.javaweb.repository.entity.BuildingEntity;
+import com.javaweb.repository.entity.DistrictEntity;
 
 public class DistrictRepositoryImpl implements DistrictRepository{
+
+	@Override
+	public List<DistrictEntity> findAll(List<BuildingEntity> buildings) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- spring-boot-project/src/main/java/com/javaweb/repository/impl/RentAreaRepositoryImpl.java
+++ spring-boot-project/src/main/java/com/javaweb/repository/impl/RentAreaRepositoryImpl.java
@@ -1,7 +1,17 @@
 package com.javaweb.repository.impl;
 
+import java.util.List;
+
 import com.javaweb.repository.RentAreaRepository;
+import com.javaweb.repository.entity.BuildingEntity;
+import com.javaweb.repository.entity.RentAreaEntity;
 
 public class RentAreaRepositoryImpl implements RentAreaRepository {
+
+	@Override
+	public List<RentAreaEntity> findAll(List<BuildingEntity> buildings) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
