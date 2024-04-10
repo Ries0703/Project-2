4c2535a32e4e060e41f499ae2a464cf954d08697
dung.ta226031@sis.hust.edu.vn <011035788Aa-+->
2024-04-10 15:57:00 +0700

    Delete DistrictRepository

--- spring-boot-project/src/main/java/com/javaweb/repository/DistrictRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javaweb.repository.entity.DistrictEntity;
-
-public interface DistrictRepository extends JpaRepository<DistrictEntity, Long> {
-
-}
